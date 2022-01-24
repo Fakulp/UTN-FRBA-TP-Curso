5cd8837d9bcf2f1bd77cd99ae9fb860972a51883
Facundo Martinez <agus.lp.md@gmail.com>
2022-01-24 15:00:29 -0300

    extended false --> true

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 const app = express();
 
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 //-------HBS--- 
--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const expressSession = require("express-session")
 router.get("/", (req, res) => {
   res.render("login");
 });
