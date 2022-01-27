dbeccde133a4b44ead5addef2c1ac4ee36833354
Facundo Martinez <agus.lp.md@gmail.com>
2022-01-27 19:35:10 -0300

    .

---
+++ models/productos.js
@@ -0,0 +1,14 @@
+// const pool = require("../db")
+
+
+// const productos = async () => {
+//     try{
+// const query = "select * from products"
+// const row = await pool.query(query)
+// return row
+//     } catch(error){
+//         console.log(error)
+//     }
+// }
+
+// module.exports = { productos }
